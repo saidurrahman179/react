078ba2216a09bdfaeae84b822db353f2ed19024c
Saidur rahman <saidurrahman179@gmail.com>
2018-11-22 15:03:26 +0600

    some change

--- admin/index.js
+++ admin/index.js
@@ -1,13 +1,6 @@
 import list from'test'
 class student extend react.component{
-
-	constractor(){
-		console.log("hahahahahahhhahah");
-	}
 	function render(argument) {
 		console.log("saiudr rahman is new in javascript")
 	}
 }
-
-
-obj= new student();
