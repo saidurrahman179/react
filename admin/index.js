bb7151d7f7e7d4566cb9e1ae4cfec4f965aa9ab7
Saidur rahman <saidurrahman179@gmail.com>
2018-11-22 16:01:31 +0600

    Thsi is final master branch

--- admin/index.js
+++ admin/index.js
@@ -1,12 +1,1 @@
-<<<<<<< HEAD
-import list from'test'
-class student extend react.component{
-	function render(argument) {
-		
-	}
-}
-
-///// this is branch aaa
-=======
-this is empty project
->>>>>>> branchB
+Ths is master Branch ok
